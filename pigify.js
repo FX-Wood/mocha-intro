1feeb29741d9c84772d7662ea40008fe119fdb70
FX-Wood <fxwood2@gmail.com>
2019-04-12 10:33:22 -0700

    Adding pigify

--- pigify.js
+++ pigify.js
@@ -1,5 +1,19 @@
 function pigify(str) {
-    return;
+    var vowels = ['a', 'e', 'i', 'o', 'u']
+    
+    if (vowels.includes(str[0])) {
+        return str + "yay"
+    }
+
+    var leadingConsonants = ''
+    
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            return str.slice(i) + leadingConsonants + 'ay'
+        } else {
+            leadingConsonants += str[i]
+        }
+    }
 }
 
 
